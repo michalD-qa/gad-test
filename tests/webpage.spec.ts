13e205033c324203333ca18e87c77e2672251566
Michal D <michal.dzialowski@gmail.com>
2023-10-23 15:27:55 +0200

    test: main pages smokes

---
+++ tests/webpage.spec.ts
@@ -0,0 +1,40 @@
+import { ArticlesPage } from '../src/pages/articles.page';
+import { CommentsPage } from '../src/pages/comments.page';
+import { HomePage } from '../src/pages/home.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify main pages', () => {
+  test('Home page title', async ({ page }) => {
+    //Arrange
+    const homePage = new HomePage(page);
+
+    //Act
+    await homePage.goto();
+
+    //Assert
+    const title = await homePage.title();
+    expect(title).toContain('GAD');
+  });
+
+  test('Articles page title', async ({ page }) => {
+    //Arrange
+    const articlesPage = new ArticlesPage(page);
+    //Act
+    await articlesPage.goto();
+
+    //Assert
+    const title = await articlesPage.title();
+    expect(title).toContain('Articles');
+  });
+
+  test('Comments page title', async ({ page }) => {
+    //Arrange
+    const commentsPage = new CommentsPage(page);
+    //Act
+    await commentsPage.goto();
+
+    //Assert
+    const title = await commentsPage.title();
+    expect(title).toContain('Comments');
+  });
+});
