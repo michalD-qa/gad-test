98da4879234c415d7fc4a785a74baa7aa3e34f43
Michal D <michal.dzialowski@gmail.com>
2023-11-07 09:22:01 +0100

    fix: remove useless file

--- tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByTestId('btn-dropdown').click();
-  await page.getByRole('link', { name: 'Login' }).click();
-});
