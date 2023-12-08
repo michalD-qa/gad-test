9f701cdf668b7df311aba306de43128804168367
Michal D <michal.dzialowski@gmail.com>
2023-12-08 10:48:35 +0100

    chore: own fixtures in smoke tests

--- tests/smoke/webpage.spec.ts
+++ tests/smoke/webpage.spec.ts
@@ -1,40 +1,27 @@
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentsPage } from '@_src/pages/comments.page';
-import { HomePage } from '@_src/pages/home.page';
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify main pages', () => {
-  test('Home page title @GAD-R01-01', async ({ page }) => {
+  test('Home page title @GAD-R01-01', async ({ homePage }) => {
     //Arrange
     const expectedHomePageTitle = 'GAD';
-    const homePage = new HomePage(page);
-
-    //Act
-    await homePage.goto();
 
     //Assert
     const title = await homePage.getTitle();
     expect(title).toContain(expectedHomePageTitle);
   });
 
-  test('Articles page title @GAD-R01-02', async ({ page }) => {
+  test('Articles page title @GAD-R01-02', async ({ articlesPage }) => {
     //Arrange
     const expectedArticlesPageTitle = 'Articles';
-    const articlesPage = new ArticlesPage(page);
-    //Act
-    await articlesPage.goto();
 
     //Assert
     const title = await articlesPage.getTitle();
     expect(title).toContain(expectedArticlesPageTitle);
   });
 
-  test('Comments page title @GAD-R01-02', async ({ page }) => {
+  test('Comments page title @GAD-R01-02', async ({ commentsPage }) => {
     //Arrange
     const expectedCommentsPageTitle = 'Comments';
-    const commentsPage = new CommentsPage(page);
-    //Act
-    await commentsPage.goto();
 
     //Assert
     const title = await commentsPage.getTitle();
