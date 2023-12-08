3513d43b8b900489f5f3aed83af94044747a740c
Michal D <michal.dzialowski@gmail.com>
2023-12-08 10:59:45 +0100

    chore: own fixtures in login.spec

--- tests/integration/login.spec.ts
+++ tests/integration/login.spec.ts
@@ -1,28 +1,15 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
 import { LoginUserModel } from '@_src/models/user.model';
-import { LoginPage } from '@_src/pages/login.page';
 import { testUser1 } from '@_src/test-data/user.data';
-import { test as base, expect } from '@playwright/test';
-
-type newFixture = {
-  loginPageNew: LoginPage;
-};
-
-const test = base.extend<newFixture>({
-  loginPageNew: async ({ page }, use) => {
-    await use(new LoginPage(page));
-  },
-});
 
 test.describe('Verify login page', () => {
   test('User can login with proper email and password @login @GAD-R02-01', async ({
-    page,
+    loginPage,
   }) => {
     //Arrange
     const expectedWelcomeTitle = 'Welcome';
-    const loginPage = new LoginPage(page);
 
     //Act
-    await loginPage.goto();
     const welcomePage = await loginPage.loginUser(testUser1);
     const title = await welcomePage.getTitle();
 
@@ -30,11 +17,10 @@ test.describe('Verify login page', () => {
     expect(title).toContain(expectedWelcomeTitle);
   });
   test("User can't login with invalid email and password @login @GAD-R02-01", async ({
-    page,
+    loginPage,
   }) => {
     //Arrange
     const expectedLoginErrorText = 'Invalid username or password';
-    const loginPage = new LoginPage(page);
 
     const loginUserData: LoginUserModel = {
       userEmail: testUser1.userEmail,
@@ -42,7 +28,6 @@ test.describe('Verify login page', () => {
     };
 
     //Act
-    await loginPage.goto();
     await loginPage.loginUser(loginUserData);
 
     //Assert
